66adc5b1e97fc5f67e6b605eed3aaaa5ce44ee6f
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-09-02 19:07:56 +0700

    setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return(
+        <nav>
+            <div>
+                <h3>NAVIGATION</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/MainPages/MainPage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/NavBar"
+
+const MainPage = () => {
+    return (
+        <div className="Mainpage">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default MainPage
