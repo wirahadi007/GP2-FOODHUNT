1e7050914b1642313e0c24b0e39e36b28bdc0a20
Ferraridiaz <87638147+Ferraridiaz@users.noreply.github.com>
2021-09-03 15:47:44 +0700

    membuat NavBar dan Header

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,10 +1,16 @@
+import React from "react";
+import { Link, Router } from "react-router-dom";
+
 const Navbar = () => {
     return(
+
         <nav>
-            <div>
-                <h3>NAVIGATION</h3>
+            <div className="flex gap-x-6 ml-4" >
+                    <p to="/Login">Login</p>
+                    <p to="/Order">Order</p>
             </div>
         </nav>
+
     )
 }
 
--- src/pages/MainPages/MainPage.jsx
+++ src/pages/MainPages/MainPage.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/Navbar/NavBar"
+import Header from "../../components/Navbar/Header"
 
 const MainPage = () => {
     return (
         <div className="Mainpage">
             <Navbar/>
+            <Header/>
         </div>
     )
 }
