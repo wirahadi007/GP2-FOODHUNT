52358cff6c5a05726539bad70a9061bdee8a921e
NeoGlanding <neoglanding@protonmail.com>
2021-09-06 14:53:48 +0700

    order card done

--- src/components/Admin Card/AdminCard.jsx
+++ src/components/Admin Card/AdminCard.jsx
@@ -1,12 +1,26 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import ModalAdmin from '../Modal/ModalAdmin/ModalAdmin';
 
 const AdminCard = props => {
     const [openModal,setOpenModal] = useState(false)
 
+
+    const approve = async (id) => {
+        await axios.put(`https://6131f19fab7b1e001799b262.mockapi.io/orders/${id}`, {
+            ...props,
+            approved: true
+        });
+
+        window.location.reload();
+    }
+
     return (
         <>
-            <div className="flex w-12/12 h-48 mb-4 justify-between bg-gray-300 rounded-lg">
+            <div className="flex w-12/12 h-48 mb-4 justify-between bg-gray-300 rounded-lg" style={{
+                backgroundColor: props.approved ? 'green' : null,
+                color: props.approved ? 'white' : null
+            }}>
                 <div className="info w-6/12 my-5 ml-6">
                     <h1 className="text-2xl font-bold">{props.name}</h1>
                     <h1 className="my-4 text-xl font-bold">REK. {props.account}</h1>
@@ -15,12 +29,15 @@ const AdminCard = props => {
                 </div>
                 <div className="self-end mr-8 mb-4">
                     <button className="bg-white px-4 py-2 rounded-lg"
+                    style={{
+                        color: props.approved ? 'black' : null
+                    }}
                     onClick={() => setOpenModal(true)}
-                    >MORE</button>
+                    >{props.approved ? 'DONE' : 'MORE'}</button>
                 </div>
             </div>
 
-            {openModal && <ModalAdmin {...props} closeModal={setOpenModal}/>}
+            {openModal && <ModalAdmin approve={approve} {...props} closeModal={setOpenModal}/>}
         </>
     )
 }
